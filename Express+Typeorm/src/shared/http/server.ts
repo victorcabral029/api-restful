30437ac32522b1e941e23f35f66b2d8b2856fecb
Amancio302 <matheusamancio.dev@gmail.com>
2022-08-19 12:16:17 -0300

    porta dinamica heroku

--- Express+Typeorm/src/shared/http/server.ts
+++ Express+Typeorm/src/shared/http/server.ts
@@ -37,6 +37,8 @@ app.use(
 	},
 );
 
-app.listen(3333, () => {
-	console.log('Server started on port 3333!');
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+	console.log('Server started on port port!');
 });
